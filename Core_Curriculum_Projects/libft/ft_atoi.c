36df66a4ca130be81b418763583d946c16ad1368
John Kaller <jkaller@c4b5c1.42berlin.de>
2023-11-22 13:45:51 +0100

    added header

--- Core_Curriculum_Projects/libft/ft_atoi.c
+++ Core_Curriculum_Projects/libft/ft_atoi.c
@@ -1,17 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaller <jkaller@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 13:40:53 by jkaller           #+#    #+#             */
+/*   Updated: 2023/11/22 13:41:17 by jkaller          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int ft_atoi(const char *str)
 {
-    int i = 0;
-    int sign_amount = 0;
-    int result = 0;
+    int i;
+    int sign_amount;
+    int result;
 
+    i = 0;
+    sign_amount = 0;
+    result = 0;
     while (str[i] != '\0')
     {
         if (ft_isspace(str[i]) == 1)
-        {
             i++;
-        }
         if (str[i] == '+')
         {
             if (sign_amount >= 1)
