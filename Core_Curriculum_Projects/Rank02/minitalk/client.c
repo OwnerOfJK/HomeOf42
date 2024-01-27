26b910327e4820e194446c59ec2e8cfb87cba286
John Kaller <john.kaller@hotmail.de>
2024-01-27 16:43:51 +0100

    final minitalk

--- Core_Curriculum_Projects/Rank02/minitalk/client.c
+++ Core_Curriculum_Projects/Rank02/minitalk/client.c
@@ -6,15 +6,53 @@
 /*   By: jkaller <jkaller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 16:46:56 by jkaller           #+#    #+#             */
-/*   Updated: 2024/01/26 16:59:30 by jkaller          ###   ########.fr       */
+/*   Updated: 2024/01/27 16:28:39 by jkaller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
-#include "minitalk.h"
+#include <unistd.h>
+#include "libft/libft.h"
 
-int	client_input(void)
+void	send_encoded_message(int pid, char *message)
 {
-	kill(server_pid, SIGUSR1);
-	kill(server_pid, SIGUSR2);
+	int	i;
+	int	j;
+
+	i = 0;
+	while (message[i] != '\0')
+	{
+		j = 0;
+		while (j < 8)
+		{
+			if (message[i] & (1 << j))
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(100);
+			j++;
+		}
+		i++;
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR2);
+		usleep(100);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		send_encoded_message(pid, argv[2]);
+		return (1);
+	}
+	else
+		ft_printf("Input Variables have an issue");
+	return (0);
 }
--- Core_Curriculum_Projects/Rank02/minitalk/server.c
+++ Core_Curriculum_Projects/Rank02/minitalk/server.c
@@ -6,29 +6,42 @@
 /*   By: jkaller <jkaller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 16:46:53 by jkaller           #+#    #+#             */
-/*   Updated: 2024/01/26 16:59:59 by jkaller          ###   ########.fr       */
+/*   Updated: 2024/01/27 16:39:34 by jkaller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include <signal.h>
-#include "minitalk.h"
+#include <unistd.h>
 
-void	signal_handler(int signum, char *message)
+void	print_decoded_message(int signum)
 {
+	static int				bit_index = 0;
+	static unsigned char	received_char = 0;
+
 	if (signum == SIGUSR1)
-		printf(message);
+		received_char |= (1 << bit_index);
 	else if (signum == SIGUSR2)
-		printf (" The random number is: %d", rand()); 
+		received_char |= (0 << bit_index);
+	bit_index++;
+	if (bit_index == 8)
+	{
+		ft_putchar_fd(received_char, 1);
+		bit_index = 0;
+		received_char = 0;
+	}
 }
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	if (argc == 1)
+	struct sigaction	action;
+
+	action.sa_handler = print_decoded_message;
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
+	ft_printf("PID of server: %d\n", getpid());
+	while (1)
 	{
-		client_input(); //do something here
-		signal_handler(SIGUSR1, argv[1]);
-		signal(SIGUSR2, signal_handler);
+		pause();
 	}
-	else
-		return (0);
 }
