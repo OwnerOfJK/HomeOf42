638efe8271ec653159d8a77f238f5c2ef22130a9
OwnerOfJK <john.kaller@hotmail.de>
2024-04-23 18:41:25 +0200

    init philosopher project

---
+++ Core_Curriculum_Projects/Rank03/philosophers/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkaller <jkaller@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 18:17:27 by jkaller           #+#    #+#             */
+/*   Updated: 2024/04/23 18:32:25 by jkaller          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	run_loop(int argc, char **argv)
+{
+	int		i;
+
+	i = argc;
+	(void)argv;
+	return ;
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 5)
+	{
+		run_loop(argc, argv);
+	}
+	return (0);
+}
