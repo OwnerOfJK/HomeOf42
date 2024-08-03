aa8db0ce3a852b54801b197c169dd16aded11c3e
OwnerOfJK <john.kaller@hotmail.de>
2024-08-03 20:54:40 +0200

    start of cpp

--- Core_Curriculum_Projects/Rank04/cpp00/ex00/megaphone.cpp
+++ Core_Curriculum_Projects/Rank04/cpp00/ex00/megaphone.cpp
@@ -12,7 +12,26 @@
 
 #include <iostream>
 
-int main(void)
+int main(int argc, char **argv)
 {
+    int i;
+    int j;
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE * \n";
+        return (0);
+    }
+    i = 1;
+    while (i < argc)
+    {
+        j = 0;
+        while (argv[i][j])
+        {
+            std::cout << (char)toupper(argv[i][j]);
+            j++;
+        }
+        i++;
+    }
+    std::cout << "\n";
     return (0);
 }
