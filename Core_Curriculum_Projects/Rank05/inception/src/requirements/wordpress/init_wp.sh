a553eb6bb5a0349374965f89f70a8275ee1d8836
OwnerOfJK <john.kaller@hotmail.de>
2024-12-14 20:49:33 +0100

    working version of inception

--- Core_Curriculum_Projects/Rank05/inception/src/requirements/wordpress/init_wp.sh
+++ Core_Curriculum_Projects/Rank05/inception/src/requirements/wordpress/init_wp.sh
@@ -4,6 +4,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=jkaller --admin_password=Jkaller42!!! --admin_email=jkaller@42.de --allow-root
+./wp-cli.phar theme install twentytwentythree --activate --allow-root
+./wp-cli.phar theme install astra --activate --allow-root
 
 php-fpm8.2 -F
