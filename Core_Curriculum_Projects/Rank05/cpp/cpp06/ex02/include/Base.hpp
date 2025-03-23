11ff2dc137a24a8332bc423b8477d1c69d14cc67
OwnerOfJK <john.kaller@hotmail.de>
2025-03-23 15:10:21 +0100

    cpp 06 ex02

---
+++ Core_Curriculum_Projects/Rank05/cpp/cpp06/ex02/include/Base.hpp
@@ -0,0 +1,23 @@
+#ifndef Base_HPP
+#define Base_HPP
+
+#include <iostream>
+
+class Base {
+    public:
+        virtual ~Base(){};
+};
+
+class A : public Base {
+
+};
+
+class B : public Base {
+
+};
+
+class C : public Base {
+
+};
+
+#endif
---
+++ Core_Curriculum_Projects/Rank05/cpp/cpp06/ex02/main.cpp
@@ -0,0 +1,67 @@
+#include "include/Base.hpp"
+#include <string>
+#include <cstdlib>
+
+Base *generate() {
+    srand((unsigned int)time(NULL)); // make sure randomness is different each time.
+    int random = rand() % 3;
+    if (random == 0) {
+        std::cout << "A instance created" << std::endl;
+        return new A();
+    } else if (random == 1) {
+        std::cout << "B instance created" << std::endl;
+        return new B();
+    } else {
+        std::cout << "C instance created" << std::endl;
+        return new C();
+    }
+    return (NULL);
+}
+
+void identify(Base *p) {
+    // Attempt to cast the pointer to each derived class using dynamic_cast.
+    if (dynamic_cast<A *>(p) != NULL) {
+        std::cout << "A" << std::endl;
+    } else if (dynamic_cast<B *>(p) != NULL) {
+        std::cout << "B" << std::endl;
+    } else if (dynamic_cast<C *>(p) != NULL) {
+        std::cout << "C" << std::endl;
+    } else {
+        std::cout << "Unknown" << std::endl;
+    }
+}
+
+void identify(Base& p)
+{
+	Base a;
+	Base b;
+	Base c;
+
+	try {
+		a = dynamic_cast<A&> (p);
+		std::cout << "A" << std::endl;
+	} catch(std::exception & e){
+    }
+	try {
+		b = dynamic_cast<B&> (p);
+		std::cout << "B" << std::endl;
+	} catch(std::exception & e){
+    }
+	try {
+		c = dynamic_cast<C&> (p);
+		std::cout << "C" << std::endl;
+	} catch(std::exception & e){
+    }
+}
+
+int main()
+{
+	Base * ptr = generate();
+	Base * ref = generate();
+
+	identify(ptr);
+	identify(*ref);
+	delete ptr;
+	delete ref;
+	return (0);
+}
