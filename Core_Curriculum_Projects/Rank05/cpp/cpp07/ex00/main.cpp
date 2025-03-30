e16c2c53f6d1d13d39f7fe3a4e223774fe2971bf
OwnerOfJK <john.kaller@hotmail.de>
2025-03-30 19:29:23 +0200

    Final fixes

--- Core_Curriculum_Projects/Rank05/cpp/cpp07/ex00/main.cpp
+++ Core_Curriculum_Projects/Rank05/cpp/cpp07/ex00/main.cpp
@@ -1,5 +1,29 @@
 #include "include/Functions.hpp"
 
+// class Point {
+//     private:
+//         int x, y;
+    
+//     public:
+//         Point(int x, int y) : x(x), y(y) {}
+    
+//         int getX() const { return x; }
+//         int getY() const { return y; }
+    
+//         bool operator<=(const Point& other) const {
+//             return (x <= other.x) && (y <= other.y);
+//         }
+    
+//         void display() const {
+//             std::cout << "(" << x << ", " << y << ")" << std::endl;
+//         }
+//     };
+
+//     std::ostream& operator<<(std::ostream& os, const Point& p) {
+//         os << "(" << p.getX() << ", " << p.getY() << ")";
+//         return os;
+//     }
+
 int main( void ) {
     int a = 2;
     int b = 3;
@@ -19,5 +43,11 @@ int main( void ) {
     std::cout << "z address: " << &z << std::endl;
     std::cout << "y address: " << &y << std::endl;
     std::cout << "x address: " << &min( z, y ) << std::endl;
+
+    // Point aComplex(3.0, 4.0);
+    // Point bComplex(5.0, 12.0);
+
+    // std::cout << "Min: " << min(aComplex, bComplex) << std::endl;
+    // std::cout << "Max: " << max(aComplex, bComplex) << std::endl;
     return 0;
 }
