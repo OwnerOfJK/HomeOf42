f4cc53a90de0652ac42c2fd7f821ad132d0110e5
OwnerOfJK <john.kaller@hotmail.de>
2025-04-17 18:37:45 +0200

    Add const and reverse iterators

--- Core_Curriculum_Projects/Rank05/cpp/cpp08/ex02/include/MutantStack.hpp
+++ Core_Curriculum_Projects/Rank05/cpp/cpp08/ex02/include/MutantStack.hpp
@@ -16,17 +16,44 @@ class MutantStack : public std::stack<T> {
             return *this;
         };
 
-        typedef typename std::stack<T>::container_type::iterator iterator;
-
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+        
         // This->c is how you're accessing the internal container
         // from within a class that inherits from std::stack.
-        iterator begin() {
-            return this->c.begin();
-        };
+		iterator begin() {
+			return this->c.begin();
+		}
 
-        iterator end() {
-            return this->c.end();
-        };
+		iterator end() {
+			return this->c.end();
+		}
+
+		const_iterator begin() const {
+			return this->c.begin();
+		}
+
+		const_iterator end() const {
+			return this->c.end();
+		}
+
+		reverse_iterator rbegin() {
+			return this->c.rbegin();
+		}
+
+		reverse_iterator rend() {
+			return this->c.rend();
+		}
+
+		const_reverse_iterator rbegin() const {
+			return this->c.rbegin();
+		}
+
+		const_reverse_iterator rend() const {
+			return this->c.rend();
+		}
 };
 
 #endif
