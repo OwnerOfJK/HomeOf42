12cae3163854a844f14caa287a8e2d7fdf746c4d
OwnerOfJK <john.kaller@hotmail.de>
2025-05-05 22:21:45 +0200

    Add overload operators to allow custom map operators

--- Core_Curriculum_Projects/Rank05/cpp/cpp09/ex00/include/BitcoinExchange.hpp
+++ Core_Curriculum_Projects/Rank05/cpp/cpp09/ex00/include/BitcoinExchange.hpp
@@ -16,6 +16,31 @@ class BitcoinExchange {
             double year;
             double month;
             double day;
+            Date(double y, double m, double d) : year(y), month(m), day(d) {}
+            bool operator<(const Date &other) const {
+                if (year != other.year)
+                    return year < other.year;
+                if (month != other.month)
+                    return month < other.month;
+                return day < other.day;
+            }
+            bool operator>(const Date &other) const {
+                if (year != other.year)
+                    return year > other.year;
+                if (month != other.month)
+                    return month > other.month;
+                return day > other.day;
+            }
+            bool operator==(const Date &other) const {
+                return year == other.year && month == other.month && day == other.day;
+            }
+            bool operator!=(const Date &other) const {
+                return !(*this == other);
+            }
+            friend std::ostream &operator<<(std::ostream &os, const Date &date) {
+                os << date.year << "-" << date.month << "-" << date.day;
+                return os;
+            }
         };
         std::map<Date, double> dataBase;
         std::multimap<Date, double> inputFile;
