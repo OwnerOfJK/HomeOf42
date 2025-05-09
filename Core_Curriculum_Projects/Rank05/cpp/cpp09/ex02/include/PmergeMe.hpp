56a39efd0d78a9b190595d1a215c87915f7cde6c
OwnerOfJK <john.kaller@hotmail.de>
2025-05-09 20:23:46 +0200

    Add merge insertion sort

--- Core_Curriculum_Projects/Rank05/cpp/cpp09/ex02/include/PmergeMe.hpp
+++ Core_Curriculum_Projects/Rank05/cpp/cpp09/ex02/include/PmergeMe.hpp
@@ -21,6 +21,19 @@ class PmergeMe {
         PmergeMe &operator=(const PmergeMe &src);
         ~PmergeMe();
 
+        // Core
+        void sort_tree();
+        //this function recursively splits the parameter array into two.
+        //returns the head of the tree
+        //recursive until the size of the arr is 1
+        void mergeSort(node *tree);
+        //will operater on subarrays with size 1
+        //look at value in each subarray, order them and put them back into the head
+        //called inside the mergeSort
+        //compare most left element of each subarray and put them one by one into the head
+        void merge(node *left_node, node *right_node, node *parent);
+
+
         // Utils
         node *copy_tree(node *tree);
         void destroy_tree(node *tree);
--- Core_Curriculum_Projects/Rank05/cpp/cpp09/ex02/src/PmergeMe.cpp
+++ Core_Curriculum_Projects/Rank05/cpp/cpp09/ex02/src/PmergeMe.cpp
@@ -49,6 +49,56 @@ void PmergeMe::destroy_tree(node *tree) {
     if (!tree) return;
     destroy_tree(tree->left);
     destroy_tree(tree->right);
-    delete[] tree->arr;
+    tree->arr.clear();
     delete tree;
+}
+
+
+void PmergeMe::sort_tree() {
+    mergeSort(this->tree);
+}
+
+void PmergeMe::mergeSort(node *tree) {
+    if (!tree) return;
+    int size = tree->arr.size();
+    if (size <= 1) return;
+    tree->left->arr.resize(size / 2);
+    tree->right->arr.resize(size - size / 2);
+    std::copy(tree->arr.begin(), tree->arr.begin() + size / 2, tree->left->arr.begin());
+    std::copy(tree->arr.begin() + size / 2 + 1, tree->arr.end() + size / 2, tree->right->arr.begin());
+    mergeSort(tree->left);
+    mergeSort(tree->right);
+    merge(tree->left, tree->right, tree);
+}
+
+void PmergeMe::merge(node *left_node, node *right_node, node *parent) {
+    int left_size = left_node->arr.size();
+    int right_size = right_node->arr.size();
+    int i = 0;
+    int l = 0;
+    int r = 0;
+
+    while (l < left_size && r < right_size)
+    {
+        if (left_node->arr[l] < right_node->arr[r]) {
+            parent->arr[i] = left_node->arr[l];
+            i++;
+            l++;
+        }
+        else {
+            parent->arr[i] = right_node->arr[r];
+            i++;
+            r++;
+        }
+    }
+    while(l < left_size) {
+        parent->arr[i] = left_node->arr[l];
+        i++;
+        l++;
+    }
+    while(r < right_size) {
+        parent->arr[i] = right_node->arr[r];
+        i++;
+        r++;
+    }
 }
