b530f9c3251f4088df4497fbfbbbb032fe9b5ea2
OwnerOfJK <john.kaller@hotmail.de>
2025-05-09 18:18:24 +0200

    Add main.cpp

---
+++ Core_Curriculum_Projects/Rank05/cpp/cpp09/ex02/main.cpp
@@ -0,0 +1,13 @@
+#include "include/PmergeMe.hpp"
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc < 2)
+        std::cout << "Not enough arguments" << std::endl;
+    int arr[argc - 1];
+    for (int i = 0; i < argc; i++) {
+        arr[i] = atoi(argv[i]);
+    }
+    PmergeMe(arr, argc -1);
+    return 0;
+}
