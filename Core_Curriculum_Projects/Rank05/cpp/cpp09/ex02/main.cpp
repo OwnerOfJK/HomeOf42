aee8105b6b4ffcdb92ed39bf4139cec9fe998314
OwnerOfJK <john.kaller@hotmail.de>
2025-05-11 21:27:07 +0200

    Add std::sort check for list

--- Core_Curriculum_Projects/Rank05/cpp/cpp09/ex02/main.cpp
+++ Core_Curriculum_Projects/Rank05/cpp/cpp09/ex02/main.cpp
@@ -1,36 +1,41 @@
 #include "include/PmergeMe.hpp"
 #include "include/MergeSort.hpp"
 #include <stdlib.h>
-#include <array>
 #include <ctime>
+#include <algorithm>
+
+#include "include/Utils.hpp"
 
 void testSort(int *arr, int size) {
-    std::cout << "\nstd::sort:" << std::endl;
+    
+    std::cout << "\nstd::sort (Vector):\n";
     std::vector<int> vecArray(arr, arr + size);
-
-    clock_t start = clock();
+    clock_t startV = clock();
     std::sort(vecArray.begin(), vecArray.end());
-    clock_t end = clock();
-    double sortDuration = static_cast<double>(end - start) * 100 / CLOCKS_PER_SEC;
+    clock_t endV = clock();
+    double sortDurationV = static_cast<double>(endV - startV) * 1000 / CLOCKS_PER_SEC;
+    printVec(vecArray);
+    std::cout << "Time: " << sortDurationV << " ms\n";
+    std::cout << "Result: " << isSortedV(vecArray) << std::endl;
 
-    if (size > 100) {
-        std::cout << "Array is too large to display." << std::endl;
-    } else {
-        std::cout << "Sorted number array:" << std::endl;
-        std::cout << "{";
-        for (int i = 0; i < size; i++) {
-            std::cout << " " << vecArray[i];
-        }
-        std::cout << " }";
-        std::cout << std::endl;
-    }
-    std::cout << "Time taken to sort the array: " << sortDuration << " milliseconds" << std::endl;
+
+    std::cout << "\nstd::sort (List):\n";
+    std::list<int> listArray(arr, arr + size);
+    clock_t startL = clock();
+    listArray.sort();
+    clock_t endL = clock();
+    double sortDurationL = static_cast<double>(endL - startL) * 1000 / CLOCKS_PER_SEC;
+    printList(listArray);
+    std::cout << "Time: " << sortDurationL << " ms\n";
+    std::cout << "Result: " << isSortedL(listArray) << std::endl;
 }
 
-
+//to test use ./PmergeMe $(shuf -i 0-10000 -n 100)
 int main(int argc, char **argv) {
-    if (argc < 2)
+    if (argc < 2) {
         std::cout << "Not enough arguments" << std::endl;
+        return 0;
+    }
     int arr[argc + 1];
     for (int i = 1; i < argc; i++) {
         arr[i - 1] = atoi(argv[i]);
@@ -49,11 +54,13 @@ int main(int argc, char **argv) {
 
     // MergeSort
     MergeSort mergeSort(arr, argc - 1);
-    mergeSort.sortNumbers();
+    mergeSort.sortVector();
+    mergeSort.sortList();
 
     // MergeInsertionSort
     PmergeMe mergeInsertion(arr, argc - 1);
-    mergeInsertion.mergeInsertionSort();
+    mergeInsertion.mergeInsertionSortVector();
+    mergeInsertion.mergeInsertionSortList();
     return 0;
 }
 
